398f5406edf3d8ed62b088ffe7c2fc68d8f8ec84
surveyving <you@example.com>
2024-05-01 16:34:50 +0600

    first commit

---
+++ js/modal.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector("#modal");
+const showMenu = document.querySelector("#menu");
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
+
+function toggleMenu() {
+  showMenu.classList.toggle("show");
+}
